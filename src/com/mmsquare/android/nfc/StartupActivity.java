ffaa77da3dc29110eaa33d5f299e3ec4c6b96cd5
unknown <floryanm@.ES.AD.ADP.com>
2011-06-20 10:17:57 +0100

    Empty Startup Activity

---
+++ src/com/mmsquare/android/nfc/StartupActivity.java
@@ -0,0 +1,11 @@
+package com.mmsquare.android.nfc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartupActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startup);
+    }
+}
