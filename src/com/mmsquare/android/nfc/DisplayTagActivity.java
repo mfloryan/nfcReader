5c6653f8567c9b5529b2cfcd7605135650b1ec04
Marcin Floryan <marcin.floryan@gmail.com>
2011-06-24 16:37:28 +0100

    Populating the tag info with some data.

--- src/com/mmsquare/android/nfc/DisplayTagActivity.java
+++ src/com/mmsquare/android/nfc/DisplayTagActivity.java
@@ -5,36 +5,55 @@ import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.widget.TextView;
+import org.w3c.dom.Text;
 
 public class DisplayTagActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.tag);
+         resolveIntent(getIntent());
+    }
 
+    private void resolveIntent(Intent intent) {
+        Tag t = getNdefTag(intent);
+
+        String[] tech = t.getTechList();
+        TextView mLabel = (TextView) findViewById(R.id_tag.text_technology);
+        TextView idLabel = (TextView) findViewById(R.id_tag.text_id);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String s:tech) {
+            sb.append(s);
+            sb.append("\n");
+        }
+
+        mLabel.setText(sb.toString());
+        idLabel.setText(t.toString());
 
     }
 
-    NdefMessage[] getNdefMessages(Intent intent) {
-        NdefMessage[] msgs = null;
+    Tag getNdefTag(Intent intent) {
+
         String action = intent.getAction();
         if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
-            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            if (rawMsgs != null) {
-                msgs = new NdefMessage[rawMsgs.length];
-                for (int i = 0; i < rawMsgs.length; i++) {
-                    msgs[i] = (NdefMessage) rawMsgs[i];
-                }
-            } else {
-                byte[] empty = new byte[]{};
-                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
-                NdefMessage msg = new NdefMessage(new NdefRecord[]{record});
-                msgs = new NdefMessage[]{msg};
-            }
 
+            Tag tag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            return tag;
         }
-        return msgs;
+        return null;
     }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+      setIntent(intent);
+      resolveIntent(intent);
+    }
+
 }
